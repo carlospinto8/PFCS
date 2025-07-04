c2b19548b72f8617155518c6e4312a44e4442b96
Carlos Pinto <123394002+carlospinto8@users.noreply.github.com>
2025-07-04 13:35:00 -0400

    feat: [PFCSDEV-5] added content and styling to login page

--- client/pfcs/src/app/app-routing.module.ts
+++ client/pfcs/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './core/home-page/home-page.component';
 import { SignUpPageComponent } from './core/sign-up-page/sign-up-page.component';
+import { LoginPageComponent } from './core/login-page/login-page.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'sign-up', component: SignUpPageComponent },
+  { path: 'login', component: LoginPageComponent },
 ];
 
 @NgModule({
--- client/pfcs/src/app/core/login-page/login-page.component.ts
+++ client/pfcs/src/app/core/login-page/login-page.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+  styleUrls: ['./login-page.component.css'],
 })
 export class LoginPageComponent {
-
+  enterAccountNumControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(16),
+    Validators.maxLength(16),
+  ]);
+  enterPasswordControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(12),
+    Validators.maxLength(64),
+    Validators.pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).+$/),
+  ]);
 }

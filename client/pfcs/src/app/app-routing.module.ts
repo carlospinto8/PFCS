3a588b9085cc8547dcf1de1ac2ae9d5940e635e6
Carlos Pinto <123394002+carlospinto8@users.noreply.github.com>
2025-07-07 13:45:16 -0400

    feat: [PFCSDEV-6] add styling and routing

--- client/pfcs/src/app/app-routing.module.ts
+++ client/pfcs/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './core/home-page/home-page.component';
 import { SignUpPageComponent } from './core/sign-up-page/sign-up-page.component';
 import { LoginPageComponent } from './core/login-page/login-page.component';
+import { UserDashboardComponent } from './core/user-dashboard/user-dashboard.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'sign-up', component: SignUpPageComponent },
   { path: 'login', component: LoginPageComponent },
+  { path: 'dashboard', component: UserDashboardComponent },
 ];
 
 @NgModule({

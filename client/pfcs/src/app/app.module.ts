4662c29b986f3387cfc773044ec606b61446ed7d
Carlos Pinto <123394002+carlospinto8@users.noreply.github.com>
2025-07-08 13:07:29 -0400

    feat: [PFCSDEV-6] added sidenav and removed header and footer form the dashboard

--- client/pfcs/src/app/app.module.ts
+++ client/pfcs/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header/header.component';
 import { SignUpPageComponent } from './core/sign-up-page/sign-up-page.component';
@@ -38,6 +39,7 @@ import { UserDashboardComponent } from './core/user-dashboard/user-dashboard.com
     MatIconModule,
     MatFormFieldModule,
     MatInputModule,
+    MatSidenavModule,
     AppRoutingModule,
     ReactiveFormsModule,
   ],

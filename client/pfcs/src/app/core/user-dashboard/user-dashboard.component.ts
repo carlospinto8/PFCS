8f444311be9dbc61c3e20993ae546e20df725267
Carlos Pinto <123394002+carlospinto8@users.noreply.github.com>
2025-07-23 21:11:43 -0400

    feat: [PFCSDEV-6] added storage amount progress bar

--- client/pfcs/src/app/core/user-dashboard/user-dashboard.component.ts
+++ client/pfcs/src/app/core/user-dashboard/user-dashboard.component.ts
@@ -5,4 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './user-dashboard.component.html',
   styleUrls: ['./user-dashboard.component.css'],
 })
-export class UserDashboardComponent {}
+export class UserDashboardComponent {
+  // TODO: init to zero and storage amount should be set dynamically after
+  //Temporary placeholder value for styling
+  storageAmountInGB: number = 0;
+  storageAmountPercentage: number = 50;
+}
